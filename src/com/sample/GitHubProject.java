a6cd9374050750ec2ad225996e34ee812d835693
unknown <farookabd5@gmail.com>
2024-04-09 21:20:40 +0530

    Fazil code added

--- src/com/sample/GitHubProject.java
+++ src/com/sample/GitHubProject.java
@@ -3,7 +3,7 @@ package com.sample;
 public class GitHubProject {
 
 	public static void main(String[] args) {
-		System.out.println("GitHub setup Completed");
+		System.out.println("Fazil GitHub setup Completed");
 
 	}
 
